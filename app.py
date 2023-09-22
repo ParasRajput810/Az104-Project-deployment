c959c39fb8e905fb04557840b9c4f491651fc76d
Paras Rajput <pr8101999@gmail.com>
2023-09-22 12:38:49 +0530

    Modifications

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[1]:
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 from azure.storage.blob import BlobServiceClient
+import string
+import random
+import requests
 import configparser
 
 app = Flask(__name__, instance_relative_config=True)
 
 Config = configparser.ConfigParser()
-Config.read("config.ini")
+Config.read("config.py")
 
 # Account name
 account = Config.get('DEFAULT', 'account')
@@ -19,33 +23,35 @@ key = Config.get('DEFAULT', 'key')
 # Container name
 container = Config.get('DEFAULT', 'container')
 
+
+# blob_service = BlockBlobService(account_name=account, account_key=key)
+
 blob_service_client = BlobServiceClient(account_url=f'https://{account}.blob.core.windows.net/', credential=key)
 
 @app.route("/")
 def main():
     return render_template('index.html')
-
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
-        if file:
-            filename = secure_filename(file.filename)
-            try:
-                blob_service_client.get_blob_client(container=container, blob=filename).upload_blob(file)
-                ref = f'https://{account}.blob.core.windows.net/{container}/{filename}'
-            except Exception as e:
-                print('Exception:', str(e))
-                ref = None
+        filename = secure_filename(file.filename)
+        fileextension = filename.rsplit('.', 1)[1]
+        try:
+            # blob_service.create_blob_from_stream(container, filename, file) 
+            # blob_service_client.create_blob_from_stream(container, filename, file)
+            blob_service_client.get_blob_client(container=container, blob=filename).upload_blob(file)
 
-    return render_template('uploadfile.html', ref=ref)
+        except Exception:
+            print('Exception=' + str(Exception))
+            pass
+        ref = 'http://' + account + '.blob.core.windows.net/' + container + '/' + filename
 
-@app.route("/home", methods=['GET'])
+    return render_template('uploadfile.html')
+
+@app.route("/home" , methods=['GET'])
 def homepage():
     return render_template('homepage.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
-
-
-# In[ ]:
