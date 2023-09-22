5b6903bbba597c820ec40194344211b975c00d5a
Paras Rajput <pr8101999@gmail.com>
2023-09-22 12:32:58 +0530

    Added some modifications

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
 [DEFAULT]
-# Account name
-account=capstonestrg
-# Azure Storage account access key
-key=eqV0jCPImDbrZ9jwBCWhMde4Gw6LxtpaJQ6RbXfms62+uDIBtoiIx855cCvzP21qAVRmF6Jq21RJ+AStEt2Dsg==
-# Container name
-container=upload
+account = capstonestrg
+key = eqV0jCPImDbrZ9jwBCWhMde4Gw6LxtpaJQ6RbXfms62+uDIBtoiIx855cCvzP21qAVRmF6Jq21RJ+AStEt2Dsg==
+container = upload
